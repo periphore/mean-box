c59e88f98a1f064bb27653e63ee7f0c4f5e01904
Hero Mosquera <maskedhero23@gmail.com>
2016-04-18 16:11:15 +0800

    Add /vagrant ownership rights check

--- .provision/dependencies.sh
+++ .provision/dependencies.sh
@@ -4,3 +4,4 @@ sudo apt-get update
 sudo apt-get install -y git build-essential curl nginx lib32stdc++6 lib32z1
 
 echo "cd /vagrant" >> /home/vagrant/.bashrc
+chown -R vagrant:vagrant /home/vagrant --quiet
